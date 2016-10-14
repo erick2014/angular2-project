7e98d90a3a3f938b7caeff3a0076a96c8805dd0c
erick2014 <erickarmandogarcia@hotmail.com>
2016-10-14 08:01:23 -0500

    Add todoApp3(styles app)

---
+++ todoApp3/app/app.component.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var AppComponent = (function () {
+    function AppComponent() {
+        this.color = "black";
+        this.bgcolor = "white";
+        this.fontSize = 12;
+    }
+    AppComponent.prototype.applyStyles = function (color, bgcolor, fontSize) {
+        console.log("applying style?", color);
+        this.color = color;
+        this.bgcolor = bgcolor;
+        this.fontSize = fontSize;
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n\t<div class=\"panel panel-default\">\n\t\t\n\t\t<div class=\"panel-heading\">\n\t\t\t<h3 class=\"panel-title\">Set Styles</h3>\n\t\t</div>\n\t\n\t\t<div class=\"panel-body\">\n\t\t\t\n\t\t\t<div class=\"form-group\">\n\t\t\t\t<label for=\"color\">Color:</label>\n\t\t\t\t<input type=\"text\" name=\"color\" value=\"{{color}}\" #colorinput />\n\t\t\t</div>\n\t\t\t\n\t\t\t<div class=\"form-group\">\n\t\t\t\t<label for=\"background\">Background Color:</label>\n\t\t\t\t<input type=\"text\" name=\"bgcolor\" value=\"{{bgcolor}}\" #bgcolorinput />\n\t\t\t</div>\n\n\t\t\t<div class=\"form-group\">\n\t\t\t\t<label for=\"background\">Font size:</label>\n\t\t\t\t<input type=\"text\" name=\"fontSize\" value=\"{{fontSize}}\" #fontinput />\n\t\t\t</div>\n\n\t\t\t<div>\n\t\t\t\t<button class=\"btn btn-default\" (click)=\"applyStyles(colorinput.value,bgcolorinput.value,fontinput.value)\">Apply Styles</button>\n\t\t\t</div>\n\n\t\t</div>\n\n\t</div>\n\n\t<div class=\"panel panel-default\">\n\n\t\t<div class=\"panel-heading\">\n\t\t\t<h3 class=\"panel-title\">Result</h3>\n\t\t</div>\n\n\t\t<div class=\"panel-body\">\n\t\t\t<div [style.font-size.px]=\"fontSize\" [ngStyle]=\"{'color':color,'background-color':bgcolor}\" align=\"center\">\n\t\t\t\tThis is the result!\n\t\t\t</div>\n\t\t</div>\n\t\n\t</div>\n\t"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ todoApp3/app/app.component.ts
@@ -0,0 +1,71 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector:'my-app',
+	template:`
+	<div class="panel panel-default">
+		
+		<div class="panel-heading">
+			<h3 class="panel-title">Set Styles</h3>
+		</div>
+	
+		<div class="panel-body">
+			
+			<div class="form-group">
+				<label for="color">Color:</label>
+				<input type="text" name="color" value="{{color}}" #colorinput />
+			</div>
+			
+			<div class="form-group">
+				<label for="background">Background Color:</label>
+				<input type="text" name="bgcolor" value="{{bgcolor}}" #bgcolorinput />
+			</div>
+
+			<div class="form-group">
+				<label for="background">Font size:</label>
+				<input type="text" name="fontSize" value="{{fontSize}}" #fontinput />
+			</div>
+
+			<div>
+				<button class="btn btn-default" (click)="applyStyles(colorinput.value,bgcolorinput.value,fontinput.value)">Apply Styles</button>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div class="panel panel-default">
+
+		<div class="panel-heading">
+			<h3 class="panel-title">Result</h3>
+		</div>
+
+		<div class="panel-body">
+			<div [style.font-size.px]="fontSize" [ngStyle]="{'color':color,'background-color':bgcolor}" align="center">
+				This is the result!
+			</div>
+		</div>
+	
+	</div>
+	`
+})
+
+export class AppComponent{
+	color: String;
+	bgcolor: String;
+	fontSize: Number;
+
+	constructor(){
+		this.color="black";
+		this.bgcolor="white";
+		this.fontSize=12;
+	}
+
+	applyStyles(color,bgcolor,fontSize){
+		console.log("applying style?",color)
+		this.color=color;
+		this.bgcolor=bgcolor;
+		this.fontSize=fontSize;
+	}
+	
+}
